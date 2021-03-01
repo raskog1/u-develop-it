4fbe0d4c46a8f656fa0c87ed4743979a1a78c74e
Ryan Skog <ryan.skog@outlook.com>
2021-03-01 11:25:12 -0800

    Added routes to get all, get one, and delete candidates

--- server.js
+++ server.js
@@ -17,6 +17,66 @@ const db = new sqlite3.Database("./db/election.db", err => {
     console.log("Connected to the election database.");
 });
 
+// GET a single candidate
+app.get("/api/candidate/:id", (req, res) => {
+    const sql = "SELECT * FROM candidates WHERE id = ?";
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: "Success",
+            data: row
+        })
+    })
+})
+
+// DELETE a single candidate
+app.delete("/api/candidate/:id", (req, res) => {
+    const sql = "DELETE FROM candidates WHERE id = ?";
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({
+            message: "Successfully deleted",
+            changes: this.changes
+        })
+    })
+})
+
+// CREATE a single candidate
+// const sql = "INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)";
+// const params = [1, "Ronald", "Firbank", 1];
+// ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, res) {
+//     if (err) console.log(err);
+//     console.log(res, this.lastID);
+// })
+
+// GET all candidates
+app.get("/api/candidates", (req, res) => {
+    const sql = "SELECT * FROM candidates";
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: "Success",
+            data: rows
+        })
+    })
+})
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
